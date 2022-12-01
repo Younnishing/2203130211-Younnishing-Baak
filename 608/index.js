e76d33c79b8f0c22e063a9f7a163731a5e498800
Younnishing Baak <114851922+Younnishing@users.noreply.github.com>
2022-12-01 15:24:21 +0800

    Add files via upload

---
+++ 608/index.js
@@ -0,0 +1,10 @@
+    alert("作者：Younnishing\n邮箱：younnishing@gmail.com\n微博：玉笙younnishing\nb站：Younnishing");
+    var Nav_1 = document.getElementById("Nav_1");
+    var Nav_2 = document.getElementById("Nav_2");
+    var Nav_3 = document.getElementById("Nav_3");
+    Nav_1.onmouseover = function (){ Nav_1.innerHTML = "之前的放映"; }
+    Nav_1.onmouseleave = function (){ Nav_1.innerHTML = "放映" }
+    Nav_2.onmouseover = function (){ Nav_2.innerHTML = "回到顶部👆"; }
+    Nav_2.onmouseleave = function (){ Nav_2.innerHTML = "👆" }
+    Nav_3.onmouseover = function (){ Nav_3.innerHTML = "关于懿笙"; }
+    Nav_3.onmouseleave = function (){ Nav_3.innerHTML = "关于" }
